e0eb0c8093d12e75c52c6219b13b13e4c97a3335
wjacker2 <wjacker2@gmail.com>
2012-01-02 18:38:10 +0000

    update html

--- project/html/GES-TEX/js/code.js
+++ project/html/GES-TEX/js/code.js
@@ -1,16 +1,25 @@
+var accordionTop = 30;
 $(function () {
-
+	$(".work").hover(function()
+	{
+		$(this).addClass("accordionHover");
+	}, function()
+    {
+		$(this).removeClass("accordionHover");
+	}); 
     $(".work").click(function () {
         var currentElement = $(this);
+		$(".work").removeClass("accordionActive");
         var currentDiv = currentElement.parent().find(".pics");
         var first = true;
         if ($(".pics:visible").length > 0 && !currentDiv.equals($(".pics:visible"))) {
             first = false;
-            $.scrollTo($(".pics:visible").parent(), 500, { onAfter: function () {
+			$(this).addClass("accordionActive");
+            $.scrollTo({ top:($(".pics:visible").parent().offset().top + accordionTop) + 'px', left:$(".pics:visible").parent().offset().left + 'px'}, 500, { onAfter: function () {
                 $(".pics:visible").slideUp(500, function () {
-                    $.scrollTo(currentElement, 500, { onAfter: function () {
+                    $.scrollTo({top:(currentElement.offset().top + accordionTop ) + 'px', left:currentElement.offset().left + 'px'}, 500, { onAfter: function () {
                         currentDiv.slideDown(500, function () {
-                            $.scrollTo(currentElement, { speed: 500 });
+                            $.scrollTo({top:(currentElement.offset().top + accordionTop ) + 'px', left:currentElement.offset().left + 'px'}, { speed: 500 });
                         });
                     } 
                     });
@@ -20,13 +29,14 @@ $(function () {
         }
 
         if (currentDiv.is(":hidden") && first) {
+			$(this).addClass("accordionActive");
             currentDiv.slideDown(500, function () {
-                $.scrollTo(currentElement, { speed: 500 });
+                $.scrollTo({top:(currentElement.offset().top + accordionTop ) + 'px', left:currentElement.offset().left + 'px'}, { speed: 500 });
             });
         }
         else if (first) {
             currentDiv.slideUp(500, function () {
-                $.scrollTo(currentElement, 500);
+                $.scrollTo({top:(currentElement.offset().top + accordionTop ) + 'px', left:currentElement.offset().left + 'px'}, 500);
             });    
         }
     });
@@ -35,7 +45,7 @@ $(function () {
 	var currentElement = $(this).parent();
 	var currentDiv = currentElement.parent().find(".work");
 	var currentPic = currentElement.parent().find(".pics");
-	$.scrollTo(currentDiv, 500, { onAfter: function () {
+	$.scrollTo({top:(currentDiv.offset().top + accordionTop ) + 'px', left:currentDiv.offset().left + 'px'}, 500, { onAfter: function () {
     currentPic.slideUp(500);
 	}});
 	});
