784317b2f8d4ef98aece755708581e0a0c316c05
wjacker2 <wjacker2@gmail.com>
2012-01-02 20:35:49 +0000

    update style

--- project/html/GES-TEX/js/code.js
+++ project/html/GES-TEX/js/code.js
@@ -1,22 +1,22 @@
 var accordionTop = 30;
 $(function () {
-	$(".work").hover(function()
+	$(".accordionHeader").hover(function()
 	{
 		$(this).addClass("accordionHover");
 	}, function()
     {
 		$(this).removeClass("accordionHover");
 	}); 
-    $(".work").click(function () {
+    $(".accordionHeader").click(function () {
         var currentElement = $(this);
-		$(".work").removeClass("accordionActive");
-        var currentDiv = currentElement.parent().find(".pics");
+		$(".accordionHeader").removeClass("accordionActive");
+        var currentDiv = currentElement.parent().find(".accordionContent");
         var first = true;
-        if ($(".pics:visible").length > 0 && !currentDiv.equals($(".pics:visible"))) {
+        if ($(".accordionContent:visible").length > 0 && !currentDiv.equals($(".accordionContent:visible"))) {
             first = false;
 			$(this).addClass("accordionActive");
-            $.scrollTo({ top:($(".pics:visible").parent().offset().top + accordionTop) + 'px', left:$(".pics:visible").parent().offset().left + 'px'}, 500, { onAfter: function () {
-                $(".pics:visible").slideUp(500, function () {
+            $.scrollTo({ top:($(".accordionContent:visible").parent().offset().top + accordionTop) + 'px', left:$(".accordionContent:visible").parent().offset().left + 'px'}, 500, { onAfter: function () {
+                $(".accordionContent:visible").slideUp(500, function () {
                     $.scrollTo({top:(currentElement.offset().top + accordionTop ) + 'px', left:currentElement.offset().left + 'px'}, 500, { onAfter: function () {
                         currentDiv.slideDown(500, function () {
                             $.scrollTo({top:(currentElement.offset().top + accordionTop ) + 'px', left:currentElement.offset().left + 'px'}, { speed: 500 });
@@ -43,8 +43,8 @@ $(function () {
 	
 	$(".close").click(function (){
 	var currentElement = $(this).parent();
-	var currentDiv = currentElement.parent().find(".work");
-	var currentPic = currentElement.parent().find(".pics");
+	var currentDiv = currentElement.parent().find(".accordionHeader");
+	var currentPic = currentElement.parent().find(".accordionContent");
 	$.scrollTo({top:(currentDiv.offset().top + accordionTop ) + 'px', left:currentDiv.offset().left + 'px'}, 500, { onAfter: function () {
     currentPic.slideUp(500);
 	}});
@@ -62,4 +62,94 @@ $.fn.equals = function (compareTo) {
         }
     }
     return true;
+}
+
+$(function() {
+	initCarousel();
+});
+//Carousel
+var carouselPanels;
+var carouselNav;
+var currentSlid;
+function initCarousel(){
+	if($("#carouselContainer").length > 0)
+	{
+		carouselPanels = $("#carouselContainer div.carouselContentWap");
+		carouselNav = $("#carousel div.carouselItem");
+		currentSlid = 0;
+		for(var i=0; i < carouselNav.length; i=i+1)   {
+			carouselNav[i].indexNum = i; 
+	
+			carouselNav[i].onclick = function() {
+				slideView(currentSlid, this.indexNum);
+			}
+		}
+		
+		$("#carousel div.preView").click(function(){
+			preView();
+		});
+		
+		$("#carousel div.nextView").click(function(){
+			nextView();
+		});
+	}
+	
+}
+
+function preView()
+{
+	if(currentSlid == 0)
+		slideView(currentSlid, carouselNav.length - 1);
+	else
+		slideView(currentSlid, currentSlid - 1);
+}
+
+function nextView()
+{
+	if(currentSlid == carouselNav.length - 1)
+		slideView(currentSlid, 0);
+	else
+		slideView(currentSlid, currentSlid + 1);
+}
+
+function slideView(fromIndex, toIndex)
+{
+	if(fromIndex == toIndex)
+		return;
+	else if(fromIndex > toIndex)
+	{
+		$(carouselPanels[fromIndex]).css("marginLeft", "0px");
+		$(carouselPanels[fromIndex]).animate({
+			marginLeft: "2000px"
+		  }, 500, function() {
+			  $(carouselPanels[fromIndex]).removeClass("active");
+			  $(carouselNav[fromIndex]).removeClass("active");
+		  });
+		  $(carouselPanels[toIndex]).css("marginLeft", "-2000px");
+		  $(carouselPanels[toIndex]).animate({
+			marginLeft: "0px"
+		  }, 500, function() {
+			 $(carouselPanels[toIndex]).addClass("active");
+			 $(carouselNav[toIndex]).addClass("active");
+		  });				
+	}
+	else
+	{
+		$(carouselPanels[fromIndex]).css("marginLeft", "0px");
+		$(carouselPanels[fromIndex]).animate({
+			marginLeft: "-2000px"
+		  }, 500, function() {
+			  $(carouselPanels[fromIndex]).removeClass("active");
+			  $(carouselNav[fromIndex]).removeClass("active");
+		  });
+		  $(carouselPanels[toIndex]).css("marginLeft", "2000px");
+		  $(carouselPanels[toIndex]).css("visibility", "visible");
+		  $(carouselPanels[toIndex]).animate({
+			marginLeft: "0px"
+		  }, 500, function() {
+			  $(carouselPanels[toIndex]).addClass("active");
+			  $(carouselNav[toIndex]).addClass("active");
+		  });
+	}
+	currentSlid = toIndex;
 }
